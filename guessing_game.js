eb0088137f78359f3e17d43a19e3b9da8b8309cd
Jahnvi <Janvi.2678@gmail.com>
2021-06-01 13:22:34 +0530

    JavaScript guess the number game
    provide a number as input and then guess the number and u will the number of guesses as result.

---
+++ guessing_game.js
@@ -0,0 +1,27 @@
+const ps = require('prompt-sync');
+const prompt = ps();
+
+let max = parseInt(prompt("Enter your maximum value"));
+while (!max) {
+    max = prompt("Enter a valid number");
+}
+
+const targetNum = Math.floor(Math.random() * max) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your guess"));
+let attempts = 1;
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too high, guess again");
+    } else {
+        guess = prompt("Too low guess again");
+    }
+}
+if (guess === 'q') {
+    console.log("you quit");
+} else {
+    console.log(`you took ${attempts} guesses`);
+}
